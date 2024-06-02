49c08f9acc686c52787832784fd8068b12b501d7
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-03 01:25:50 +0530

    Scroll Trigger

--- scrollTrigger/script.js
+++ scrollTrigger/script.js
@@ -13,6 +13,32 @@ gsap.from('#page2 .box', {
     opacity: 0,
     scrollTrigger: {
         trigger: '#page2 .box',
+        markers: true,
+        scroller: "body",
+        start: 'top 60%'
+    }
+})
+gsap.from("#page3 h1", {
+    x: 500,
+    opacity: 0,
+    duration: 1, 
+    delay: 1,
+    scrollTrigger: {
+        trigger: "#page3 h1",
+        start: "top 50%",
+        scroller: "body",
         markers: true
     }
+})
+
+gsap.from("#page3 h2", {
+    x: -500,
+    opacity: 0,
+    duration: 1, 
+    delay: 1,
+    scrollTrigger: {
+        trigger: "#page3 h2",
+        scroller: "body",
+    }
+
 })
