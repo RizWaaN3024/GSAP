639b2797383b13f5a4eafa30b8d441b4da76a293
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-04 01:48:51 +0530

    Learnt SVG animation

---
+++ svg/script.js
@@ -0,0 +1,20 @@
+var path = "M 10 100 Q 500 100 990 100"
+var finalPath = "M 10 100 Q 500 100 990 100"
+
+var string = document.querySelector('#string')
+string.addEventListener('mousemove', function(dets) {
+    path = `M 10 100 Q ${dets.x} ${dets.y} 990 100`
+    gsap.to('svg path', {
+        attr: {d: path},
+        duration: 0.5,
+        ease: "power3.out"
+    })
+})
+
+string.addEventListener("mouseleave", function() {
+    gsap.to("svg path", {
+        attr: {d:finalPath},
+        duration:1.5,
+        ease: "elastic.out(1,0.2)"
+    })
+})
