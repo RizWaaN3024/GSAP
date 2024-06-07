e870eab4bea7890c2f3cf9940090c5ba0e6cd99f
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-08 02:28:04 +0530

    Issue Fix

--- text-animation/script.js
+++ text-animation/script.js
@@ -3,4 +3,17 @@ var h1Text = h1.textContent;
 
 var splittedText = h1Text.split("");
 
-console.log(splittedText);
+var clutter = "";
+
+splittedText.forEach(function(e) {
+    clutter = clutter + `<span>${e}</span>`
+})
+h1.innerHTML = clutter;
+
+gsap.from('span', {
+    y:100,
+    duration:1,
+    delay:1,
+    opacity: 0,
+    stagger: 0.2
+})
